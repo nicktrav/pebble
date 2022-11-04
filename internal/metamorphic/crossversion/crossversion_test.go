958293022d9abeef528ad188629e81d1ad820576
Nick Travers <travers@cockroachlabs.com>
2022-11-04 15:32:21 -0700

    metamorphic/crossversion: update reproduction command
    Update the reproduction command for the crossversion test to make use of
    the wrapper script. Previously the command would output the command with
    the paths to the test binaries, which may not exist on the machine where
    the reproduction is attempted.
    
    Allow the seed and the branching factor to be passed through as
    environment variables.
    
    As a result, the output should allow for a simple copy-paste to perform
    a reproduction. For example:
    
    ```
    $ SEED=123 FACTOR=1 ./scripts/run-crossversion-meta.sh crl-release-21.2 crl-release-22.1 crl-release-22.2 master
    ...
        crossversion_test.go:83: Reproduction:
              SEED=123 FACTOR=1 ./scripts/run-crossversion-meta.sh 441910e2 03e49132 0fc8d9e0 f2a7c319
    ```
    
    Touches #2099.

--- internal/metamorphic/crossversion/crossversion_test.go
+++ internal/metamorphic/crossversion/crossversion_test.go
@@ -64,8 +64,10 @@ moved on failure. Defaults to the current working directory.`)
 }
 
 func reproductionCommand() string {
-	return fmt.Sprintf("go test -v -run 'TestMetaCrossVersion' --seed %d --factor %d %s\n",
-		seed, factor, versions.String())
+	return fmt.Sprintf(
+		"SEED=%d FACTOR=%d ./scripts/run-crossversion-meta.sh %s\n",
+		seed, factor, versions.String(),
+	)
 }
 
 // TestMetaCrossVersion performs cross-version metamorphic testing.
@@ -300,7 +302,7 @@ func (f *pebbleVersions) String() string {
 		if i > 0 {
 			fmt.Fprint(&buf, " ")
 		}
-		fmt.Fprintf(&buf, "--version %s", v.String())
+		fmt.Fprintf(&buf, v.SHA)
 	}
 	return buf.String()
 }
